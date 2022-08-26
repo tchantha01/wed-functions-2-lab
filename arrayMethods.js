e2c163763f06fa6e57f05013cf9b78a9f95b2fe1
Toky Chanthavong <tchantha01@gmail.com>
2022-08-25 20:35:31 -0600

    fun2 arrays

--- arrayMethods.js
+++ arrayMethods.js
@@ -18,9 +18,11 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 // CODE HERE
-const evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+const evenNumbers = mixedNumbers.filter(function(num){
+  return num % 2 === 0
+})
 
-
+console.log(evenNumbers)
 
 ////////// PROBLEM 2 //////////
 
@@ -39,7 +41,8 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 // CODE HERE
-const postTaxPrices // = prices.map(/* Provide Your Callback Here );
+
+const postTaxPrices = prices.map(num => num * 1.07 );
 
 
 
